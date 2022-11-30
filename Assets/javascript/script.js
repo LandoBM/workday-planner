a2d7143666d3dd23c3d0d4de5b29b74de720fa98
Landon Murray <landonbmurray@icloud.com>
2022-11-29 23:27:02 -0500

    final commit

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(() => {
     // Moment Script for Day
     var today = moment().format('dddd, MMM D, YYYY');
@@ -23,34 +21,34 @@ $(document).ready(() => {
     $('#10 .description').val(localStorage.getItem('10'))
     $('#11 .description').val(localStorage.getItem('11'))
     $('#12 .description').val(localStorage.getItem('12'))
-    $('#13 .description').val(localStorage.getItem('1'))
-    $('#14 .description').val(localStorage.getItem('2'))
-    $('#15 .description').val(localStorage.getItem('3'))
-    $('#16 .description').val(localStorage.getItem('4'))
-    $('#16 .description').val(localStorage.getItem('5'))
-    $('#17 .description').val(localStorage.getItem('6'))
+    $('#13 .description').val(localStorage.getItem('13'))
+    $('#14 .description').val(localStorage.getItem('14'))
+    $('#15 .description').val(localStorage.getItem('15'))
+    $('#16 .description').val(localStorage.getItem('16'))
+    $('#17 .description').val(localStorage.getItem('17'))
+    $('#18 .description').val(localStorage.getItem('18'))
 
     function colorAdjust() {
         var currentTime = moment().hours()
 
-        $('.time-block').each(() => {
-            var filledTime = parseInt($(this).attr('id'))
-                console.log(filledTime)
+        $('.time-block').each((index, element) => {
+            var filledTime = parseInt($(element).attr('id'))
+                console.log(filledTime, currentTime)
         
             if (currentTime < filledTime) {
+                $(this).addClass("past")
                 $(this).removeClass("future")
                 $(this).removeClass("present")
-                $(this).addClass("past")
             } else if (filledTime === currentTime) {
                 $(this).removeClass("past")
-                $(this).removeClass("future")
                 $(this).addClass("present")
+                $(this).removeClass("future")
             } else {
                 $(this).removeClass("past")
                 $(this).removeClass("present")
                 $(this).addClass("future")
             }
         })
-    }
+    } 
     colorAdjust()
 })
